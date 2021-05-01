0c80a68e838b864a357c2bb5f204b204f5f9bead
Yeltsin <yeltsinrhp.93@gmail.com>
2021-04-30 19:08:31 -0500

    INICIALIZACION MIGRACION A DB DOCKER

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,5 +1,8 @@
+using BusinessLogic.Data.Conexion;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -11,9 +14,25 @@ namespace WebApi
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+
+                try
+                {
+                    var context = services.GetRequiredService<ZafamiDbContext>();
+                    await context.Database.MigrateAsync();
+                }catch(Exception e)
+                {
+                    var logger = loggerFactory.CreateLogger<Program>();
+                    logger.LogError(e, "Errores en el proceso de migracion");
+                }
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
